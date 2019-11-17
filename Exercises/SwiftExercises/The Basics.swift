b763e2948e4590cb5c5e99cac1718c997f4d9dc7
Chunngai <mikumikku3939@icloud.com>
2019-11-17 23:46:57 +0800

    exercise of the basics

---
+++ Exercises/SwiftExercises/The Basics.swift
@@ -0,0 +1,49 @@
+/* Constants and Variables */
+// Declaring Constants and Variables
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+/* Type Annotations */
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+/* Naming Constants and Variables */
+let n = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+// let languageName = "Swift"
+// languageName = "Swift++"
+
+/* Printing Constants and Variables */
+print(friendlyWelcome)
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+/* Comments */
+// This is a comment
+
+/* This is also a comment
+but is written over multiple lines */
+
+/* This is the start of the first multiline comment.
+/* This is the second, nested multiline comment. */
+This is the end of the first multiline comment. */
+
+/* Semicolons */
+let cat = "🐱"; print(cat)
+
+/* Integers */
+// Integer Bounds
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+// Int
