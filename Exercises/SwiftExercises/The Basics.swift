cfed8e823bd588250efae8820acc756f3ae3e40b
Chunngai <mikumikku3939@icloud.com>
2019-11-19 01:18:39 +0800

    the basics exercise

--- Exercises/SwiftExercises/The Basics.swift
+++ Exercises/SwiftExercises/The Basics.swift
@@ -74,5 +74,104 @@ let oneMillion = 1_000_000
 let justOverOneMillion = 1_000_000.000_000_1
 
 /* Numeric Type Conversion */
+// Integer Conversion
+// let cannotBeNegative: UInt8 = -1
+// let tooBig: Int8 = Int8.max + 1
 
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+// Integer and Floating-Point Conversion
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+
+let integerPi = Int(pi)
+
+/* Type Aliases */
+typealias AudioSample = UInt16
+
+var maxAmplitudeFound = AudioSample.min
+
+/* Booleans */
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+	print("Mmm, tasty turnips!")
+} else {
+	print("Eww, turnips are horrible.")
+}
+
+/*
+let i = 1
+if i {
+	// ...
+}
+*/
+
+let i = 1
+if i == 1 {
+	// ...
+}
+
+/* Tuples */
+let http404Error = (404, "Not Found")
+
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+print("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
+
+/* Optionals */
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+// nil
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var surveyAnswer: String?
+
+// If Statements and Forced Unwrapping
+if convertedNumber != nil {
+	print("convertedNumber contains some integer value.")
+}
+
+if convertedNumber != nil {
+	print("convertedNumber has an integer value of \
+	(convertedNumber!).")
+}
+
+// Optional Binding
+if let actualNumber = Int(possibleNumber) {
+	print("The string \"\(possibleNumber)\" has an integer value 
+	of \(actualNumber)")
+} else {
+	print("The string \"\(possibleNumber)\" could not be 
+	converted to an integer")
+	
+if let firstNumber = Int("4"), let secondNumber = Int("42"), 
+		firstNumber < secondNumber && secondNumber < 100 {
+	print("\(firstNumber) < \(secondNumber) < 100")
+}
+
+if let firstNumber = Int("4") {
+	if let secondNumber = Int("42") {
+		if firstNumber < secondNumber && secondNumber < 100 {
+			print("\(firstNumber) < \(secondNumber) < 100")
+		}
+	}
+}
 
