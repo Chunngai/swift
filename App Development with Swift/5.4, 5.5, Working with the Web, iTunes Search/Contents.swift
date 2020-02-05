5443e38fa044e86a04d1de771c9bb4f74bbc075c
Chunngai <mikumikku3939@icloud.com>
2020-02-05 21:57:51 +0800

    u4, u5

---
+++ App Development with Swift/5.4, 5.5, Working with the Web, iTunes Search/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let query: ["String": "String"] = ["term": "jack johnson"]
+
+let baseURL = "https://itunes.apple.com/search?"
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map {
+            URLQueryItem(name: $0.0, value: $0.1)
+        }
+        return components?.url
+    }
+}
+
+let url = baseURL.withQueries(query)
+
+let task = URLSession.shared.dataTask(with: url) { (data, response, error) in 
+    if let data = data, let string = String(data: data, encoding: .utf8) {
+        print(string)
+    }
+    PlaygroundPage.current.finishExecution()
+}
+task.resume()
